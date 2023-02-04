fe105c5ce0c45f122e927b9aef1545b8123c728d
ishivanshgoel <shivanshgoel21@gmail.com>
2023-02-05 02:55:00 +0530

    complete controllers

--- src/main/java/com/stonepaperscissor/controller/GameController.java
+++ src/main/java/com/stonepaperscissor/controller/GameController.java
@@ -1,47 +1,56 @@
 package com.stonepaperscissor.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.stonepaperscissor.payloads.CreateNewGameDto;
-import com.stonepaperscissor.payloads.CreateNewGameResponse;
-import com.stonepaperscissor.payloads.PlayGameDto;
-import com.stonepaperscissor.payloads.PlayGameResponse;
+import com.stonepaperscissor.payloads.game.CreateNewGameDto;
+import com.stonepaperscissor.payloads.game.CreateNewGameResponse;
+import com.stonepaperscissor.payloads.game.PlayGameDto;
+import com.stonepaperscissor.payloads.game.PlayGameResponse;
+import com.stonepaperscissor.service.GameService;
 
 @RestController
+@RequestMapping("/api/v1/game")
 public class GameController {
+	
+	@Autowired
+	private GameService gameService;
 	
 	/**
 	 * starts a new game
 	 * @return CreateNewGameResponse
 	 */
-	@PostMapping("/game/new")
+	@PostMapping("/new")
 	public ResponseEntity<CreateNewGameResponse> createNewGame(@RequestBody CreateNewGameDto createNewGameDto) {
-		return new ResponseEntity<CreateNewGameResponse> (new CreateNewGameResponse(), HttpStatus.CREATED);
+		CreateNewGameResponse createNewGameResponse = this.gameService.createNewGameAndAddUser(createNewGameDto);
+		return new ResponseEntity<CreateNewGameResponse> (createNewGameResponse, HttpStatus.CREATED);
 	}
 	
 	/**
 	 * @param gameId unique identifier for the game
-	 * @param playGameDto paramters required as input to play the game
+	 * @param playGameDto parameters required as input to play the game
 	 * @return PlayGameResponse
 	 */
-	@PostMapping("/game/{gameId}/play")
+	@PostMapping("/{gameId}/play")
 	public ResponseEntity<PlayGameResponse> playGame(@PathVariable String gameId, @RequestBody PlayGameDto playGameDto) {
-		return new ResponseEntity<PlayGameResponse> (new PlayGameResponse(), HttpStatus.OK);
+		PlayGameResponse playGameResponse = this.gameService.playGame(gameId, playGameDto);
+		return new ResponseEntity<PlayGameResponse> (playGameResponse, HttpStatus.OK);
 	}
 	
 	/**
 	 * @param gameId a unique identifier for the game
 	 * @return returns the winner of game (Player wins/ Computer wins/ It is a tie).
 	 */
-	@GetMapping("/game/{gameId}/winner")
+	@GetMapping("/{gameId}/winner")
 	public ResponseEntity<String> getWinnerOfGame(@PathVariable String gameId) {
-		System.out.println("Game Id " + gameId);
-		return new ResponseEntity<String> ("Computer Wins", HttpStatus.OK);
+		String winner = this.gameService.getGameWinner(gameId);
+		return new ResponseEntity<String> (winner, HttpStatus.OK);
 	}
 }
--- src/main/java/com/stonepaperscissor/controller/UserController.java
+++ src/main/java/com/stonepaperscissor/controller/UserController.java
@@ -1,16 +1,28 @@
 package com.stonepaperscissor.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.stonepaperscissor.payloads.user.CreateNewUserDto;
+import com.stonepaperscissor.payloads.user.CreateNewUserResponse;
+import com.stonepaperscissor.service.UserService;
 
 @RestController
 public class UserController {
-	/**
-	 * register new user
-	 * @return
+	@Autowired
+	private UserService userService;
+	
+	/***
+	 * @param CreateNewUserDto
+	 * @return CreateNewUserResponse
 	 */
 	@PostMapping("/user/add")
-	public Boolean addUser() {
-		return true;
+	public ResponseEntity<CreateNewUserResponse> addUser(@RequestBody CreateNewUserDto createNewUserDto) {
+		CreateNewUserResponse createNewUserResponse = this.userService.registerNewUser(createNewUserDto);
+		return new ResponseEntity<CreateNewUserResponse> (createNewUserResponse, HttpStatus.CREATED);
 	}
 }
